e47cada4e51903f4d72434ae90282cfbb7fdaafa
Alessandro Lemos Jr <ablemosjunior@gmail.com>
2026-02-26 11:41:42 -0300

    use removeAttr

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { removeDataTest } from "./vite-plugin-remove-data-test";
+import removeAttr from 'remove-attr'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,7 +13,10 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
-    mode === "production" && removeDataTest(),
+    removeAttr({
+      extensions: ["ts", "tsx", "js", "jsx"],
+      attributes: ["data-test", "data-testid"],
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
